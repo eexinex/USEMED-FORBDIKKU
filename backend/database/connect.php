10ba8facee99640485d5576b54205c38ff139e05
Narawit Sandorn <nsandorn7@gmail.com>
2026-06-02 02:08:18 +0700

    Update connect.php

--- backend/database/connect.php
+++ backend/database/connect.php
@@ -39,6 +39,7 @@ function db(): ?PDO
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_PERSISTENT => true, // Use persistent connections for speed
             PDO::ATTR_TIMEOUT => 5, // 5 seconds timeout to prevent hanging
         ]);
 
